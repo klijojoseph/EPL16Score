960dc16a1e8c6388f06ddcdcf979dfcad8b745ab
Bibin Babu <bibinbabu.mj@gmail.com>
2016-08-19 11:55:08 +0530

    Initial commit by bibin

--- app/src/main/java/com/lijojoseph/epl16score/MainActivity.java
+++ app/src/main/java/com/lijojoseph/epl16score/MainActivity.java
@@ -1,31 +1,28 @@
 package com.lijojoseph.epl16score;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Spinner team1Spinner,team2Spinner;
+    private Button submitBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        //initalize id
+        team1Spinner=(Spinner)findViewById(R.id.team1_spinner);
+        team1Spinner=(Spinner)findViewById(R.id.team1_spinner);
+        submitBtn=(Button)findViewById(R.id.submit_btn);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
